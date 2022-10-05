9167af64e0dfabea6a8fef69f5c5e325139e3f67
Alexis Perin <alexis.perin@hotmail.com>
2022-10-05 17:32:10 +0200

    Update ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 17:21:21 by aperin            #+#    #+#             */
-/*   Updated: 2022/10/05 17:21:53 by aperin           ###   ########.fr       */
+/*   Updated: 2022/10/05 17:30:13 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new_lst;
+	t_list	*tmp;
+
+	new_lst = 0;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&new_lst, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_lst, tmp);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
